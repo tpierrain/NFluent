e845db0a0d9784a8c9298456c0ce2240238329d0
Thomas PIERRAIN <thomas@pierrain.net>
2013-02-17 00:54:23 +0100

    Adds the Assert class + tests.

---
+++ NFluent.Tests/AssertThatTests.cs
@@ -0,0 +1,29 @@
+﻿namespace NFluent.Tests
+{
+    using NUnit.Framework;
+    using Assert = NFluent.Assert; // alias needed here since NUnit uses the same name; but useless for other unit test frameworks.
+
+    [TestFixture]
+    public class AssertThatTests
+    {
+        [Test]
+        public void AssertThatWorks()
+        {
+            Assert.That(true);
+        }
+        
+        [Test]
+        [ExpectedException(typeof(FluentAssertionException))]
+        public void AssertThatThowsExceptionWhenFails()
+        {
+            Assert.That(false);
+        }
+
+        [Test]
+        [ExpectedException(typeof(FluentAssertionException), ExpectedMessage = "Dumbest test ever")]
+        public void AssertThatThowsExceptionWithIndicatedMessageWhenFails()
+        {
+            Assert.That(false, "Dumbest test ever");
+        }
+    }
+}
---
+++ NFluent/Assert.cs
@@ -0,0 +1,34 @@
+﻿namespace NFluent
+{
+    /// <summary>
+    /// Contains a collection of static methods that implement the only assertions you need to be fluent (if your favorite .NET unit test framework doesn't provide it).
+    /// If you are using NUnit, this <see cref="T:NFluent.Assert"/> class is useless (since NUnit provides now few Assert.That() overloads).
+    /// On the other hand, xUnit users may benefit to this class.
+    /// </summary>
+    public static class Assert
+    {
+        /// <summary>
+        /// Asserts that a condition is true. If the condition is false the method throw a <see cref="T:FluentAssertionException"/>.
+        /// </summary>
+        /// <param name="condition">The evaluated condition.</param>
+        public static void That(bool condition)
+        {
+            // TODO: improve the default message of failing Assert.That
+            That(condition, string.Empty);
+        }
+
+        /// <summary>
+        /// Asserts that a condition is true. If the condition is false the method throw a <see cref="T:FluentAssertionException" />.
+        /// </summary>
+        /// <param name="condition">The evaluated condition.</param>
+        /// <param name="message">The message for the <see cref="FluentAssertionException"/> if assertion fails.</param>
+        /// <exception cref="FluentAssertionException">The assertion fails.</exception>
+        public static void That(bool condition, string message)
+        {
+            if (!condition)
+            {
+                throw new FluentAssertionException(message);
+            }
+        }
+    }
+}
