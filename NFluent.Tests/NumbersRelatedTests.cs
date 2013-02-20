509fbccf58e92271a7ac4be4ed204ccc198a62cd
Thomas PIERRAIN <thomas@pierrain.net>
2013-02-20 01:56:05 +0100

    Adds 2 new  NumberRelatedExtensions methods (IsZero() and IsNotZero) + tests

--- NFluent.Tests/NumbersRelatedTests.cs
+++ NFluent.Tests/NumbersRelatedTests.cs
@@ -10,12 +10,32 @@
     [TestFixture]
     public class NumbersRelatedTests
     {
+        #region IsZero
+
         [Test]
         public void IsZeroWorks()
         {
-            int a = 0;
+            int intValue = 0;
+            long longValue = 0L;
+            double doubleValue = 0D;
+            decimal decimalValue = 0M;
+            float floatValue = 0F;
+            short shortValue = 0;
+            byte byteValue = 0;
+            uint uintValue = 0;
+            ushort ushortValue = 0;
+            ulong ulongValue = 0;
 
-            Assert.That(a.IsZero());
+            Assert.That(intValue.IsZero());
+            Assert.That(longValue.IsZero());
+            Assert.That(doubleValue.IsZero());
+            Assert.That(decimalValue.IsZero());
+            Assert.That(floatValue.IsZero());
+            Assert.That(shortValue.IsZero());
+            Assert.That(byteValue.IsZero());
+            Assert.That(uintValue.IsZero());
+            Assert.That(ushortValue.IsZero());
+            Assert.That(ulongValue.IsZero());
         }
 
         [Test]
@@ -26,5 +46,57 @@
 
             Assert.That(a.IsZero());
         }
+
+        #endregion 
+
+        #region IsNotZero
+
+        [Test]
+        public void IsNotZeroWorks()
+        {
+            int intValue = 2;
+            long longValue = 2L;
+            double doubleValue = 2D;
+            decimal decimalValue = 2M;
+            float floatValue = 2F;
+            short shortValue = 2;
+            byte byteValue = 2;
+            uint uintValue = 2;
+            ushort ushortValue = 2;
+            ulong ulongValue = 2;
+
+            Assert.That(intValue.IsNotZero());
+            Assert.That(longValue.IsNotZero());
+            Assert.That(doubleValue.IsNotZero());
+            Assert.That(decimalValue.IsNotZero());
+            Assert.That(floatValue.IsNotZero());
+            Assert.That(shortValue.IsNotZero());
+            Assert.That(byteValue.IsNotZero());
+            Assert.That(uintValue.IsNotZero());
+            Assert.That(ushortValue.IsNotZero());
+            Assert.That(ulongValue.IsNotZero());
+        }
+
+        [Test]
+        [ExpectedException(typeof(FluentAssertionException), ExpectedMessage = "[0] is equal to zero.")]
+        public void IsNotZeroThrowsExceptionWhenFails()
+        {
+            double a = 0D;
+
+            Assert.That(a.IsNotZero());
+        }
+
+        #endregion 
+
+        #region IsPositive
+
+        //// [Test]
+        //// public void isPositiveWorks()
+        //// {
+        ////    int a = 2;
+        ////    Assert.That(a.IsPositive());
+        //// }
+
+        #endregion
     }
 }
--- NFluent/NumbersRelatedExtension.cs
+++ NFluent/NumbersRelatedExtension.cs
@@ -27,19 +27,105 @@ namespace NFluent
         /// <summary>
         /// Verifies that the actual value is equal to zero.
         /// </summary>
+        /// <typeparam name="T">Type of the actual value.</typeparam>
         /// <param name="value">The value to check.</param>
         /// <returns>
-        ///   <c>true</c> if the specified value is zero; throws a <see cref="FluentAssertionException"/> otherwise.
+        ///   <c>true</c> if the specified value is equal to zero; throws a <see cref="FluentAssertionException" /> otherwise.
         /// </returns>
         /// <exception cref="FluentAssertionException">The value is not equal to zero.</exception>
-        public static bool IsZero(this int value)
+        public static bool IsZero<T>(this T value)
         {
-            if (!int.Equals(0, value))
+            bool res = InternalIsZero(value);
+
+            if (!res)
             {
                 throw new FluentAssertionException(string.Format("[{0}] is not equal to zero.", value));
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// Verifies that the actual value is NOT equal to zero.
+        /// </summary>
+        /// <typeparam name="T">Type of the actual value.</typeparam>
+        /// <param name="value">The value to check.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified value is NOT equal to zero; throws a <see cref="FluentAssertionException" /> otherwise.
+        /// </returns>
+        /// <exception cref="FluentAssertionException">The value is equal to zero.</exception>
+        public static bool IsNotZero<T>(this T value)
+        {
+            bool res = InternalIsZero(value);
+
+            if (res)
+            {
+                throw new FluentAssertionException(string.Format("[{0}] is equal to zero.", value));
+            }
+
+            return true;
+        }
+
+        //// public static bool IsPositive<T>(this T value)
+        //// {
+        ////    if (value < 0)
+        ////    {
+        ////        throw new FluentAssertionException(string.Format("[{0}] is not positive.", value));
+        ////    }
+        ////    return true;
+        //// }
+
+        /// <summary>
+        /// Checks whether a given value is equal to zero.
+        /// </summary>
+        /// <typeparam name="T">Type of the value.</typeparam>
+        /// <param name="value">The value.</param>
+        /// <returns><c>true</c> if the specified value is equal to zero; false otherwise.</returns>
+        private static bool InternalIsZero<T>(T value)
+        {
+            bool res = false;
+            if (value is int)
+            {
+                res = int.Equals(value, 0);
+            }
+            else if (value is long)
+            {
+                res = long.Equals(value, 0L);
+            }
+            else if (value is double)
+            {
+                res = double.Equals(value, 0D);
+            }
+            else if (value is decimal)
+            {
+                res = decimal.Equals(value, 0M);
+            }
+            else if (value is float)
+            {
+                res = float.Equals(value, 0F);
+            }
+            else if (value is short)
+            {
+                res = short.Equals(value, (short)0.0);
+            }
+            else if (value is byte)
+            {
+                res = byte.Equals(value, (byte)0);
+            }
+            else if (value is uint)
+            {
+                res = uint.Equals(value, (uint)0);
+            }
+            else if (value is ushort)
+            {
+                res = ushort.Equals(value, (ushort)0);
+            }
+            else if (value is ulong)
+            {
+                res = ulong.Equals(value, (ulong)0);
+            }
+
+            return res;
         }
     }
 }
