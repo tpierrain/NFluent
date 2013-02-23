41de38d8d367c086ce4a77c8fea1210b35787949
Thomas PIERRAIN <thomas@pierrain.net>
2013-02-23 15:08:20 +0100

    Adds the string.Contains and StartWith extension methods + tests.

---
+++ NFluent.Tests/StringRelatedTests.cs
@@ -0,0 +1,51 @@
+﻿namespace NFluent.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Text;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class StringRelatedTests
+    {
+        [Test]
+        public void ContainsWork()
+        {
+            var alphabet = "abcdefghijklmnopqrstuvwxyz";
+            Assert.That(alphabet.Contains("c", "z", "u"));
+        }
+
+        [Test]
+        [ExpectedException(typeof(FluentAssertionException), ExpectedMessage = @"The string [""abcdefghijklmnopqrstuvwxyz""] does not contain the expected value(s): [""0"", ""4""].")]
+        public void ContainsThrowsExceptionWhenFails()
+        {
+            var alphabet = "abcdefghijklmnopqrstuvwxyz";
+            Assert.That(alphabet.Contains("c", "0", "4"));
+        }
+
+        [Test]
+        [ExpectedException(typeof(FluentAssertionException), ExpectedMessage = @"The string [""abcdefghijklmnopqrstuvwxyz""] does not contain the expected value(s): [""C"", ""A""].")]
+        public void ContainsIsCaseSensitive()
+        {
+            var alphabet = "abcdefghijklmnopqrstuvwxyz";
+            Assert.That(alphabet.Contains("C", "a", "A", "z"));
+        }
+
+        [Test]
+        public void StartWithWorks()
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            Assert.That(alphabet.StartWith("abcdef"));
+        }
+
+        [Test]
+        [ExpectedException(typeof(FluentAssertionException), ExpectedMessage = @"The string [""abcdefghijklmnopqrstuvwxyz""] does not start with [""ABCDEF""].")]
+        public void StartWithIsCaseSensitive()
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            Assert.That(alphabet.StartWith("ABCDEF"));
+        }
+    }
+}
