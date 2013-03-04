10857ecb13f1ad7112ccc844dc794abb100f56a0
Thomas PIERRAIN <thomas@pierrain.net>
2013-03-04 08:32:42 +0100

    Removes useless using.

--- NFluent/EnumerableFluentAssert.cs
+++ NFluent/EnumerableFluentAssert.cs
@@ -17,7 +17,6 @@ namespace NFluent
     using System;
     using System.Collections;
     using System.Collections.Generic;
-    using System.Collections.Specialized;
     using System.Linq;
 
     
