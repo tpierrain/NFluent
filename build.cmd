666b09a021aad1c9b5f0da9299a48098cc0993dd
Thomas PIERRAIN <thomas@pierrain.net>
2013-03-18 01:31:02 +0100

    Includes the buld.cmd within the solution items
    Sets Release as the default configuration option for the build.cmd script.

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@
 cls
 set config=%1
 if "%config%" == "" (
-   set config=debug
+   set config=release
 )
 
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild %~dp0\.build\Build.proj /p:Configuration="%config%" /t:RunAll /v:M /fl /flp:LogFile=.\.build\msbuild.log;Verbosity=Normal /nr:false
