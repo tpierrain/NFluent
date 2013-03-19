ddf18909a627de99587dcba7ac0e77485d9bd8c3
Thomas PIERRAIN <thomas@pierrain.net>
2013-03-19 07:34:29 +0100

    Updates the metadata of the NFluent assembly.

--- NFluent/Properties/AssemblyInfo.cs
+++ NFluent/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NFluent")]
 [assembly: AssemblyDescription("Smooth your .NET TDD experience with NFluent! NFluent is an ergonomic assertion library which aims to fluent your .NET TDD experience (based on simple Check.That() assertion statements). NFluent aims your tests to be fluent to write (with an happy 'dot' auto completion experience), fluent to read (i.e. as close as possible to plain English expression), but also fluent to troubleshoot, in a less-error-prone way comparing to the classical .NET test frameworks. NFluent is also directly inspired by the awesome Java FEST Fluent assertion/reflection library (http://fest.easytesting.org/)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Thomas PIERRAIN (thomas@pierrain.net)")]
+[assembly: AssemblyCompany("Thomas PIERRAIN (thomas@pierrain.net), Marc-Antoine LATOUR and contributors.")]
 [assembly: AssemblyProduct("NFluent")]
 [assembly: AssemblyCopyright("Copyright ©  2013. Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0).")]
 [assembly: AssemblyTrademark("")]
