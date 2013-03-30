5a6293d9405c314acf17fe108ce04ce72422f343
Thomas PIERRAIN <thomas@pierrain.net>
2013-03-30 08:36:59 +0100

    Hides the Sut property of the Spike.Core.IFluentAssertion from the intellisense via the [EditorBrowsable(EditorBrowsableState.Never)] attribute.
    Note that it won't work if Resharper is wrongly configured as explained here: http://stackoverflow.com/questions/4441298/why-editorbrowsable-doesnt-work

--- NFluent.Tests/ExtensionsSpike/Core/IFluentAssertion.cs
+++ NFluent.Tests/ExtensionsSpike/Core/IFluentAssertion.cs
@@ -1,7 +1,10 @@
 ﻿namespace Spike.Core
 {
+    using System.ComponentModel;
+
     public interface IFluentAssertion<out T>
     {
+        [EditorBrowsable(EditorBrowsableState.Never)]
         T Sut { get; }
     }
 }
--- NFluent.Tests/ExtensionsSpike/Core/ObjectFluentAssertion.cs
+++ NFluent.Tests/ExtensionsSpike/Core/ObjectFluentAssertion.cs
@@ -3,6 +3,7 @@
     public class ObjectFluentAssertion : IFluentAssertion<object>
     {
         public object Sut { get; private set; }
+
         public ObjectFluentAssertion(object sut)
         {
             this.Sut = sut;
