0b34ff8020f107b12ed74697910f7f101f5f4915
Cyrille Dupuydauby <dupdob@gmail.com>
2013-04-15 06:45:07 +0200

    untracked files on master: d364069 Merge commit 'd8d30c87932d6f07a890c90ca064b89d59dddbdc'

---
+++ NFluent.Tests/TimeSpanTests.cs
@@ -0,0 +1,39 @@
+﻿// // --------------------------------------------------------------------------------------------------------------------
+// // <copyright file="TimeSpanTests.cs" company="">
+// //   Copyright 2013 Cyrille DUPUYDAUBY
+// //   Licensed under the Apache License, Version 2.0 (the "License");
+// //   you may not use this file except in compliance with the License.
+// //   You may obtain a copy of the License at
+// //       http://www.apache.org/licenses/LICENSE-2.0
+// //   Unless required by applicable law or agreed to in writing, software
+// //   distributed under the License is distributed on an "AS IS" BASIS,
+// //   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// //   See the License for the specific language governing permissions and
+// //   limitations under the License.
+// // </copyright>
+// // --------------------------------------------------------------------------------------------------------------------
+namespace NFluent.Tests
+{
+    using System;
+
+    using NFluent.Helpers;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TimeSpanTests
+    {
+        [Test]
+        public void LessThanTest()
+        {
+            Check.That(TimeSpan.FromMilliseconds(500)).IsLessThan(100, TimeUnit.Milliseconds);
+        }
+
+        [Test]
+        [ExpectedException(typeof(FluentAssertionException), UserMessage = "Should have failed")]
+        public void LessThanTestFails()
+        {
+            Check.That(TimeSpan.FromMilliseconds(50)).IsLessThan(100, TimeUnit.Milliseconds);
+        }
+    }
+}
