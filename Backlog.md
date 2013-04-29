5bc080481582f01337d153c54bb91fa40d1dfa95
Thomas PIERRAIN <thomas@pierrain.net>
2013-04-29 08:13:14 +0200

    Adds more unit tests related to the Not operator.

--- Backlog.md
+++ Backlog.md
@@ -8,6 +8,8 @@ Now available on github: https://github.com/tpierrain/NFluent/issues?state=open
 Temporary backlog
 -------
 
+1. Implement 'Not' for every assertion method
+1. Update md documentation to indicate that it is mandatory to implement std and negated versions for every new assertion method
 1. Use T4 to generate Extensions methods for numbers?
 1. Clean up some code within the {x}Extensions files?
 1. Ensure test coverage is 100%
--- NFluent.Tests/NotRelatedTests.cs
+++ NFluent.Tests/NotRelatedTests.cs
@@ -17,5 +17,18 @@
         {
             Check.That("Batman and Robin").Not.Contains("Batman");
         }
+
+        [Test]
+        public void CanCombineNotAndAndOperatorsInTheSameCheckStatement()
+        {
+            Check.That("Batman and Robin").Not.Contains("Joker").And.StartsWith("Bat").And.Not.Contains("Gandhi");
+        }
+
+        [Test]
+        [ExpectedException(typeof(FluentAssertionException), ExpectedMessage = "\nThe actual string:\n\t[\"Batman and Robin\"]\n contains the value(s):\n\t[\"Robin\"]\nwhich was not expected.")]
+        public void ThrowsProperExceptionWhenCombineNotAndAndOperatorsInTheSameCheckStatement()
+        {
+            Check.That("Batman and Robin").Not.Contains("Joker").And.StartsWith("Bat").And.Not.Contains("Robin");
+        }
     }
 }
