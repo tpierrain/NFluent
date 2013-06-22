5d88dd0fc101f0db61b31ad791b6fd5d733a6ff0
Thomas PIERRAIN <thomas@pierrain.net>
2013-06-23 00:38:27 +0200

    Now, the NumberChecks does not implement ICheck and Co anymore. That was useless, and prevented us to have a good test coverage.

--- NFluent/Assertions/NumberCheck.cs
+++ NFluent/Assertions/NumberCheck.cs
@@ -15,19 +15,19 @@
 namespace NFluent
 {
     using System;
-    using System.Diagnostics.CodeAnalysis;
     using NFluent.Helpers;
 
     /// <summary>
     /// Provides check methods to be executed on a number instance.
     /// </summary>
     /// <typeparam name="N">Type of the numerical value.</typeparam>
-    public class NumberCheck<N> : ICheck<N>, IRunnableCheck<N>, ICheckRunner<N> where N : IComparable
+    public class NumberCheck<N> where N : IComparable
     {
         private const string MustBeZeroMessage = "The {0} is different from zero.";
 
-        private readonly ICheck<N> check;
+        // private readonly N value;
         private readonly CheckRunner<N> checkRunner;
+        private IRunnableCheck<N> runnableCheck;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="NumberCheck{N}" /> class.
@@ -35,87 +35,9 @@ namespace NFluent
         /// <param name="check">The fluent check.</param>
         public NumberCheck(ICheck<N> check)
         {
-            this.check = check;
-            this.checkRunner = new CheckRunner<N>(this);
-        }
-
-        /// <summary>
-        /// Gets the value to be tested (provided for any extension method to be able to test it).
-        /// </summary>
-        /// <value>
-        /// The value to be tested by any fluent check extension method.
-        /// </value>
-        public N Value 
-        { 
-            get
-            {
-                return ((IRunnableCheck<N>)this.check).Value;
-            }
-        }
-
-        /// <summary>
-        /// Gets a value indicating whether this <see cref="FluentCheck{T}" /> should be negated or not.
-        /// </summary>
-        /// <value>
-        ///   <c>true</c> if all the methods applying to this check instance should be negated; <c>false</c> otherwise.
-        /// </value>
-        public bool Negated { get; private set; }
-        
-        /// <summary>
-        /// Negates the next check.
-        /// </summary>
-        /// <value>
-        /// The next check negated.
-        /// </value>
-        [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1623:PropertySummaryDocumentationMustMatchAccessors", Justification = "Reviewed. Suppression is OK here since we want to trick and improve the auto-completion experience here.")]
-        public ICheck<N> Not { get; private set; }
-
-        /// <summary>
-        /// Executes the check provided as an happy-path lambda (vs lambda for negated version).
-        /// </summary>
-        /// <param name="action">The happy-path action (vs. the one for negated version which has not to be specified). This lambda should simply return if everything is ok, or throws a <see cref="FluentCheckException"/> otherwise.</param>
-        /// <param name="negatedExceptionMessage">The message for the negated exception.</param>
-        /// <returns>
-        /// A new check link.
-        /// </returns>
-        /// <exception cref="FluentCheckException">The check fails.</exception>
-        ICheckLink<ICheck<N>> ICheckRunner<N>.ExecuteCheck(Action action, string negatedExceptionMessage)
-        {
-            return this.checkRunner.ExecuteCheck(action, negatedExceptionMessage);
-        }
-
-        /// <summary>
-        /// Creates a new instance of the same fluent check type, injecting the same Value property
-        /// (i.e. the system under test), but with a false Negated property in any case.
-        /// </summary>
-        /// <returns>
-        /// A new instance of the same fluent check type, with the same Value property.
-        /// </returns>
-        /// <remarks>
-        /// This method is used during the chaining of multiple checks.
-        /// </remarks>
-        public object ForkInstance()
-        {
-            return new NumberCheck<N>(this.check);
-        }
-
-        /// <summary>
-        /// Checks whether the specified <see cref="System.Object" /> is equal to this instance or not.
-        /// </summary>
-        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
-        /// <returns>
-        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; throws a <see cref="FluentCheckException"/> otherwise.
-        /// </returns>
-        /// <exception cref="FluentCheckException">The specified <see cref="System.Object"/> is not equal to this instance.</exception>
-        public new bool Equals(object obj)
-        {
-            var checkRunner = this as ICheckRunner<N>;
-
-            checkRunner.ExecuteCheck(
-                () => EqualityHelper.IsEqualTo(this.Value, obj),
-                EqualityHelper.BuildErrorMessage(this.Value, obj, true));
-
-            return true;
+            // this.value = ((IRunnableCheck<N>)check).Value;
+            this.checkRunner = new CheckRunner<N>(check as IRunnableCheck<N>);
+            this.runnableCheck = check as IRunnableCheck<N>;
         }
 
         /// <summary>
@@ -127,17 +49,14 @@ namespace NFluent
         /// <exception cref="FluentCheckException">The value is not equal to zero.</exception>
         public ICheckLink<ICheck<N>> IsZero()
         {
-            var checkRunner = this.check as ICheckRunner<N>;
-            IRunnableCheck<N> runnableCheck = this;
-
-            return checkRunner.ExecuteCheck(
+            return this.checkRunner.ExecuteCheck(
                 () =>
                     {
-                        var res = InternalIsZero(runnableCheck.Value);
+                        var res = InternalIsZero(this.runnableCheck.Value);
 
                         if (!res)
                         {
-                            throw new FluentCheckException(FluentMessage.BuildMessage(MustBeZeroMessage).On(runnableCheck.Value).ToString());
+                            throw new FluentCheckException(FluentMessage.BuildMessage(MustBeZeroMessage).On(this.runnableCheck.Value).ToString());
                         }
                     },
                 FluentMessage.BuildMessage("The {0} is equal to zero whereas it must not.").ToString());
@@ -152,10 +71,7 @@ namespace NFluent
         /// <exception cref="FluentCheckException">The value is equal to zero.</exception>
         public ICheckLink<ICheck<N>> IsNotZero()
         {
-            var checkRunner = this.check as ICheckRunner<N>;
-            IRunnableCheck<N> runnableCheck = this;
-
-            return checkRunner.ExecuteCheck(
+            return this.checkRunner.ExecuteCheck(
                 () =>
                     {
                         bool res = InternalIsZero(runnableCheck.Value);
@@ -164,11 +80,11 @@ namespace NFluent
                         {
                             throw new FluentCheckException(
                                 FluentMessage.BuildMessage("The {0} is equal to zero, whereas it must not.")
-                                             .On(runnableCheck.Value)
+                                             .On(this.runnableCheck.Value)
                                              .ToString());
                         }
                     },
-                FluentMessage.BuildMessage("The {0} is different from zero.").On(runnableCheck.Value).ToString());
+                FluentMessage.BuildMessage("The {0} is different from zero.").On(this.runnableCheck.Value).ToString());
         }
 
         /// <summary>
@@ -178,18 +94,15 @@ namespace NFluent
         /// <exception cref="FluentCheckException">The value is not strictly positive.</exception>
         public ICheckLink<ICheck<N>> IsPositive()
         {
-            var checkRunner = this.check as ICheckRunner<N>;
-            IRunnableCheck<N> runnableCheck = this;
-
-            return checkRunner.ExecuteCheck(
+            return this.checkRunner.ExecuteCheck(
                 () =>
                     {
-                        if (Convert.ToInt32(runnableCheck.Value) <= 0)
+                        if (Convert.ToInt32(this.runnableCheck.Value) <= 0)
                         {
-                            throw new FluentCheckException(FluentMessage.BuildMessage("The {0} is not strictly positive.").On(runnableCheck.Value).ToString());
+                            throw new FluentCheckException(FluentMessage.BuildMessage("The {0} is not strictly positive.").On(this.runnableCheck.Value).ToString());
                         }
                     },
-                FluentMessage.BuildMessage("The {0} is positive, whereas it must not.").On(runnableCheck.Value).ToString());
+                FluentMessage.BuildMessage("The {0} is positive, whereas it must not.").On(this.runnableCheck.Value).ToString());
         }
 
         /// <summary>
@@ -206,18 +119,15 @@ namespace NFluent
         /// </exception>
         public ICheckLink<ICheck<N>> IsLessThan(N comparand)
         {
-            var checkRunner = this.check as ICheckRunner<N>;
-            IRunnableCheck<N> runnableCheck = this;
-
-            return checkRunner.ExecuteCheck(
+            return this.checkRunner.ExecuteCheck(
                 () =>
                 {
                     if (runnableCheck.Value.CompareTo(comparand) >= 0)
                     {
-                        throw new FluentCheckException(FluentMessage.BuildMessage("The {0} is greater than the threshold.").On(runnableCheck.Value).And.Expected(comparand).Comparison("less than").ToString());
+                        throw new FluentCheckException(FluentMessage.BuildMessage("The {0} is greater than the threshold.").On(this.runnableCheck.Value).And.Expected(comparand).Comparison("less than").ToString());
                     }
                 },
-                FluentMessage.BuildMessage("The {0} is less than the threshold.").On(runnableCheck.Value).And.Expected(comparand).Comparison("more than").ToString());
+                FluentMessage.BuildMessage("The {0} is less than the threshold.").On(this.runnableCheck.Value).And.Expected(comparand).Comparison("more than").ToString());
         }
 
         /// <summary>
@@ -234,18 +144,15 @@ namespace NFluent
         /// </exception>
         public ICheckLink<ICheck<N>> IsGreaterThan(N comparand)
         {
-            var checkRunner = this.check as ICheckRunner<N>;
-            IRunnableCheck<N> runnableCheck = this;
-
-            return checkRunner.ExecuteCheck(
+            return this.checkRunner.ExecuteCheck(
                 () =>
                     {
                         if (runnableCheck.Value.CompareTo(comparand) <= 0)
                         {
-                            throw new FluentCheckException(FluentMessage.BuildMessage("The {0} is less than the threshold.").On(runnableCheck.Value).And.Expected(comparand).Comparison("more than").ToString());
+                            throw new FluentCheckException(FluentMessage.BuildMessage("The {0} is less than the threshold.").On(this.runnableCheck.Value).And.Expected(comparand).Comparison("more than").ToString());
                         }
                     },
-                FluentMessage.BuildMessage("The {0} is greater than the threshold.").On(runnableCheck.Value).And.Expected(comparand).Comparison("less than").ToString());
+                FluentMessage.BuildMessage("The {0} is greater than the threshold.").On(this.runnableCheck.Value).And.Expected(comparand).Comparison("less than").ToString());
         }
 
         #region IEqualityFluentAssertion members
@@ -258,15 +165,12 @@ namespace NFluent
         /// <exception cref="FluentCheckException">The actual value is not equal to the expected value.</exception>
         public ICheckLink<ICheck<N>> IsEqualTo(object expected)
         {
-            var checkRunner = this.check as ICheckRunner<N>;
-            IRunnableCheck<N> runnableCheck = this;
-
-            return checkRunner.ExecuteCheck(
+            return this.checkRunner.ExecuteCheck(
                 () =>
                     {
-                        EqualityHelper.IsEqualTo(runnableCheck.Value, expected);
+                        EqualityHelper.IsEqualTo(this.runnableCheck.Value, expected);
                     },
-                EqualityHelper.BuildErrorMessage(runnableCheck.Value, expected, true));
+                EqualityHelper.BuildErrorMessage(this.runnableCheck.Value, expected, true));
         }
 
         /// <summary>
@@ -277,15 +181,12 @@ namespace NFluent
         /// <exception cref="FluentCheckException">The actual value is equal to the expected value.</exception>
         public ICheckLink<ICheck<N>> IsNotEqualTo(object expected)
         {
-            var checkRunner = this.check as ICheckRunner<N>;
-            IRunnableCheck<N> runnableCheck = this;
-
-            return checkRunner.ExecuteCheck(
+            return this.checkRunner.ExecuteCheck(
                 () =>
                     {
-                        EqualityHelper.IsNotEqualTo(runnableCheck.Value, expected);
+                        EqualityHelper.IsNotEqualTo(this.runnableCheck.Value, expected);
                     },
-                EqualityHelper.BuildErrorMessage(runnableCheck.Value, expected, false));
+                EqualityHelper.BuildErrorMessage(this.runnableCheck.Value, expected, false));
         }
 
         #endregion
@@ -302,15 +203,12 @@ namespace NFluent
         /// <exception cref="FluentCheckException">The actual instance is not of the provided type.</exception>
         public ICheckLink<ICheck<N>> IsInstanceOf<T>()
         {
-            var checkRunner = this.check as ICheckRunner<N>;
-            var runnableCheck = this;
-
-            return checkRunner.ExecuteCheck(
+            return this.checkRunner.ExecuteCheck(
                 () =>
                 {
-                    IsInstanceHelper.IsInstanceOf(runnableCheck.Value, typeof(T));
+                    IsInstanceHelper.IsInstanceOf(this.runnableCheck.Value, typeof(T));
                 },
-                IsInstanceHelper.BuildErrorMessage(runnableCheck, typeof(T), true));
+                IsInstanceHelper.BuildErrorMessage(this.runnableCheck, typeof(T), true));
         }
 
         /// <summary>
@@ -323,15 +221,12 @@ namespace NFluent
         /// <exception cref="FluentCheckException">The actual instance is of the provided type.</exception>
         public ICheckLink<ICheck<N>> IsNotInstanceOf<T>()
         {
-            var checkRunner = this.check as ICheckRunner<N>;
-            var runnableCheck = this;
-
-            return checkRunner.ExecuteCheck(
+            return this.checkRunner.ExecuteCheck(
                 () =>
                 {
-                    IsInstanceHelper.IsNotInstanceOf(runnableCheck.Value, typeof(T));
+                    IsInstanceHelper.IsNotInstanceOf(this.runnableCheck.Value, typeof(T));
                 },
-                IsInstanceHelper.BuildErrorMessage(runnableCheck, typeof(T), false));
+                IsInstanceHelper.BuildErrorMessage(this.runnableCheck, typeof(T), false));
         }
 
         #endregion
