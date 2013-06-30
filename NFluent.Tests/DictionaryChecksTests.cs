d15dbe54183efa68096feb9a4e4a5f47ea1902ec
Cyrille Dupuydauby <dupdob@gmail.com>
2013-06-30 15:53:28 +0200

    Implemented ContainsKey

---
+++ NFluent.Tests/DictionaryChecksTests.cs
@@ -0,0 +1,59 @@
+﻿// // --------------------------------------------------------------------------------------------------------------------
+// // <copyright file="DictionaryChecksTests.cs" company="">
+// //   Copyright 2013 Cyrille DUPUYDAUBY
+// //   Licensed under the Apache License, Version 2.0 (the "License");
+// //   you may not use this file except in compliance with the License.
+// //   You may obtain a copy of the License at
+// //       http://www.apache.org/licenses/LICENSE-2.0
+// //   Unless required by applicable law or agreed to in writing, software
+// //   distributed under the License is distributed on an "AS IS" BASIS,
+// //   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// //   See the License for the specific language governing permissions and
+// //   limitations under the License.
+// // </copyright>
+// // --------------------------------------------------------------------------------------------------------------------
+
+namespace NFluent.Tests
+{
+    using System.Collections.Generic;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DictionaryChecksTests
+    {
+        private static readonly Dictionary<string, string> SimpleDico;
+
+        static DictionaryChecksTests()
+        {
+            SimpleDico = new Dictionary<string, string>();
+            SimpleDico["demo"] = "value";
+        }
+
+        [Test]
+        public void ContainsKeyWorks()
+        {
+            Check.That(SimpleDico).ContainsKey("demo");
+        }
+
+        [Test]
+        [ExpectedException(typeof(FluentCheckException), ExpectedMessage = "\nThe checked Dictionary does not contain the expected key.\nThe checked Dictionary:\n\t[[demo, value]]\nExpected key:\n\t[\"value\"]")]
+        public void ContainsKeyFailsProperly()
+        {
+            Check.That(SimpleDico).ContainsKey("value");
+        }
+
+        [Test]
+        public void NotContainsKeyWorksProperly()
+        {
+            Check.That(SimpleDico).Not.ContainsKey("value");
+        }
+
+        [Test]
+        [ExpectedException(typeof(FluentCheckException), ExpectedMessage = "\nThe checked Dictionary does contain the key, whereas it must not.\nThe checked Dictionary:\n\t[[demo, value]]\nExpected key:\n\t[\"demo\"]")]
+        public void NotContainsKeyFailsProperly()
+        {
+            Check.That(SimpleDico).Not.ContainsKey("demo");
+        }
+    }
+}
---
+++ NFluent/Assertions/DictionaryCheckExtension.cs
@@ -0,0 +1,77 @@
+﻿// // --------------------------------------------------------------------------------------------------------------------
+// // <copyright file="DictionaryCheckExtension.cs" company="">
+// //   Copyright 2013 Cyrille DUPUYDAUBY
+// //   Licensed under the Apache License, Version 2.0 (the "License");
+// //   you may not use this file except in compliance with the License.
+// //   You may obtain a copy of the License at
+// //       http://www.apache.org/licenses/LICENSE-2.0
+// //   Unless required by applicable law or agreed to in writing, software
+// //   distributed under the License is distributed on an "AS IS" BASIS,
+// //   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// //   See the License for the specific language governing permissions and
+// //   limitations under the License.
+// // </copyright>
+// // --------------------------------------------------------------------------------------------------------------------
+namespace NFluent
+{
+    using System.Collections;
+
+    using NFluent.Helpers;
+
+    /// <summary>
+    /// Provides check methods to be executed on an <see cref="IDictionary"/> value.
+    /// </summary>
+    public static class DictionaryCheckExtension
+    {
+        /// <summary>
+        /// Checks that the actual <see cref="IDictionary"/> contains the expected key.
+        /// </summary>
+        /// <typeparam name="K">
+        /// The type of the key element.
+        /// </typeparam>
+        /// <param name="check">
+        /// The fluent check to be extended.
+        /// </param>
+        /// <param name="key">
+        /// The expected key value.
+        /// </param>
+        /// <returns>
+        /// A check link.
+        /// </returns>
+         public static ICheckLink<ICheck<IDictionary>> ContainsKey<K>(this ICheck<IDictionary> check, K key)
+         {
+            var checkItem = check as IRunnableCheck<IDictionary>;
+            var checkedDico = checkItem.Value;
+            var negated = checkItem.Negated;
+            string message = null;
+            if (checkedDico.Contains(key) == negated)
+            {
+                if (negated)
+                {
+                    message =
+                        FluentMessage.BuildMessage("The {0} does contain the key, whereas it must not.")
+                                     .For("Dictionary")
+                                     .On(checkedDico)
+                                     .And.Expected(key)
+                                     .Label("Expected key:").ToString();
+                }
+                else
+                {
+                    message =
+                        FluentMessage.BuildMessage("The {0} does not contain the expected key.")
+                                     .For("Dictionary")
+                                     .On(checkedDico)
+                                     .And.Expected(key)
+                                     .Label("Expected key:").ToString();   
+                }
+            }
+
+             if (message != null)
+             {
+                 throw new FluentCheckException(message);
+             }
+
+            return new CheckLink<ICheck<IDictionary>>(check);
+         }
+    }
+}
