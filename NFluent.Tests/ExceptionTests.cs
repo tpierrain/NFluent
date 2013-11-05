330a5d83319e1463189b46ad4314654743f16f5e
Thomas PIERRAIN <thomas@pierrain.net>
2013-11-05 08:07:46 +0100

    Extracts the Exception tests from the LambdaRelatedTests.cs file, and put them in a new dedicated ExceptionTests.

---
+++ NFluent.Tests/ExceptionTests.cs
@@ -0,0 +1,50 @@
+﻿namespace NFluent.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ExceptionTests
+    {
+        [Test]
+        public void NoExceptionRaised()
+        {
+            Check.That(() => new object()).DoesNotThrow();
+        }
+
+        [Test]
+        [ExpectedException(typeof(FluentCheckException), MatchType = MessageMatch.StartsWith, ExpectedMessage = "\nThe checked code raised an exception, whereas it must not.")]
+        public void UnexpectedExceptionRaised()
+        {
+            Check.That(() => { throw new ApplicationException(); }).DoesNotThrow();
+        }
+
+        [Test]
+        public void ExpectedExceptionRaised()
+        {
+            Check.That(() => { throw new InvalidOperationException(); }).Throws<InvalidOperationException>();
+            Check.That(() => { throw new ApplicationException(); }).ThrowsAny();
+        }
+
+        [Test]
+        [ExpectedException(typeof(FluentCheckException), MatchType = MessageMatch.Contains, ExpectedMessage = "\nThe checked code raised an exception of a different type than expected.")]
+        public void DidNotRaiseExpected()
+        {
+            Check.That(() => { throw new Exception(); }).Throws<ApplicationException>();
+        }
+
+        [Test]
+        [ExpectedException(typeof(FluentCheckException), ExpectedMessage = "\nThe checked code did not raise an exception, whereas it must.")]
+        public void DidNotRaiseAny()
+        {
+            Check.That(() => { new object(); }).ThrowsAny();
+        }
+
+        [Test]
+        [ExpectedException(typeof(FluentCheckException), ExpectedMessage = "\nThe checked code did not raise an exception, whereas it must.\nExpected exception type is:\n\t[System.Exception]")]
+        public void DidNotRaiseAnyTypedCheck()
+        {
+            Check.That(() => { new object(); }).Throws<Exception>();
+        }
+    }
+}
--- NFluent.Tests/LambdaRelatedTests.cs
+++ NFluent.Tests/LambdaRelatedTests.cs
@@ -14,7 +14,6 @@
 // // --------------------------------------------------------------------------------------------------------------------
 namespace NFluent.Tests
 {
-    using System;
     using System.Threading;
 
     using NUnit.Framework;
@@ -22,6 +21,8 @@ namespace NFluent.Tests
     [TestFixture]
     public class LambdaRelatedTests
     {
+        private readonly ExceptionTests exceptionTests = new ExceptionTests();
+
         [Test]
         public void DurationTest()
         {
@@ -33,47 +34,6 @@ namespace NFluent.Tests
         public void FailDurationTest()
         {
             Check.That(() => Thread.Sleep(0)).LastsLessThan(0, TimeUnit.Milliseconds);
-        }
-
-        [Test]
-        public void NoExceptionRaised()
-        {
-            Check.That(() => new object()).DoesNotThrow();
-        }
-
-        [Test]
-        [ExpectedException(typeof(FluentCheckException), MatchType = MessageMatch.StartsWith, ExpectedMessage = "\nThe checked code raised an exception, whereas it must not.")]
-        public void UnexpectedExceptionRaised()
-        {
-            Check.That(() => { throw new ApplicationException(); }).DoesNotThrow();
-        }
-
-        [Test]
-        public void ExpectedExceptionRaised()
-        {
-            Check.That(() => { throw new InvalidOperationException(); }).Throws<InvalidOperationException>();
-            Check.That(() => { throw new ApplicationException(); }).ThrowsAny();
-        }
-
-        [Test]
-        [ExpectedException(typeof(FluentCheckException), MatchType = MessageMatch.Contains, ExpectedMessage = "\nThe checked code raised an exception of a different type than expected.")]
-        public void DidNotRaiseExpected()
-        {
-            Check.That(() => { throw new Exception(); }).Throws<ApplicationException>();
-        }
-
-        [Test]
-        [ExpectedException(typeof(FluentCheckException), ExpectedMessage = "\nThe checked code did not raise an exception, whereas it must.")]
-        public void DidNotRaiseAny()
-        {
-            Check.That(() => { new object(); }).ThrowsAny();
-        }
-
-        [Test]
-        [ExpectedException(typeof(FluentCheckException), ExpectedMessage = "\nThe checked code did not raise an exception, whereas it must.\nExpected exception type is:\n\t[System.Exception]")]
-        public void DidNotRaiseAnyTypedCheck()
-        {
-            Check.That(() => { new object(); }).Throws<Exception>();
         }
     }
 }
