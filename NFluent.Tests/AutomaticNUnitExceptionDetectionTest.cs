4c80eea803295288b352b30b2aa5d0df82850dc3
Thomas PIERRAIN <thomas@pierrain.net>
2013-11-13 01:13:02 +0100

    Fixes header with proper names.

--- NFluent.Tests/AutomaticNUnitExceptionDetectionTest.cs
+++ NFluent.Tests/AutomaticNUnitExceptionDetectionTest.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="AutomaticNUnitExceptionDetectionTest.cs" company="">
-//   Copyright 2013 Cyrille DUPUYDAUBY
+//   Copyright 2013 Cyrille DUPUYDAUBY, Rui CARVALHO, Thomas PIERRAIN
 //   // //   Licensed under the Apache License, Version 2.0 (the "License");
 //   // //   you may not use this file except in compliance with the License.
 //   // //   You may obtain a copy of the License at
