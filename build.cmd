71d1c4f82431490b7d26fff5d243a5f5d49bde90
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2016-09-22 13:26:03 +0200

    Initiate fixing building script (+initiate  T4 multiproject support)

--- build.cmd
+++ build.cmd
@@ -5,4 +5,4 @@ if "%config%" == "" (
    set config=release
 )
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild %~dp0\.build\Build.proj /p:Configuration="%config%" /t:RunAll /v:M /fl /flp:LogFile=.\.build\msbuild.log;Verbosity=Diagnostic /nr:false
+msbuild %~dp0\.build\Build.proj /p:Configuration="%config%" /t:RunAll /v:M /fl /flp:LogFile=.\.build\msbuild.log;Verbosity=Diagnostic /nr:false
