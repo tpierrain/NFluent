a3dc4400bb0303904c484b0f3cee5d287266d2ad
Thomas PIERRAIN <thomas@pierrain.net>
2017-04-12 20:34:48 +0200

    Fix build (which now is adapted to the latest msbuild and VS2107-based projects and tools)

--- build.cmd
+++ build.cmd
@@ -5,4 +5,5 @@ if "%config%" == "" (
    set config=release
 )
 
-msbuild .\.build\Build.proj /p:Configuration="%config%" /t:RunAll /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Diagnostic /nr:false
+REM msbuild .\.build\Build.proj /p:Configuration="%config%" /t:RunAll /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Diagnostic /nr:false
+msbuild .\.build\Build.proj /p:Configuration="%config%" /t:Package /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Diagnostic /nr:false
