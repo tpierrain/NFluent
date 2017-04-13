5f9e3d3f3170c5902718424fa0f689ef9c057ba9
Thomas PIERRAIN <thomas@pierrain.net>
2017-04-13 16:02:58 +0200

    Make the build.cmd work in another directory + delete lame DemoProject

--- build.cmd
+++ build.cmd
@@ -5,5 +5,14 @@ if "%config%" == "" (
    set config=release
 )
 
+set targetPath=C:\projects\nfluent\
+set initialPath=%cd%
+
+git clone -q %initialPath% %targetPath%
+
+cd %targetPath%
+
 REM msbuild .\.build\Build.proj /p:Configuration="%config%" /t:RunAll /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Diagnostic /nr:false
 msbuild .\.build\Build.proj /p:Configuration="%config%" /t:Package /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Diagnostic /nr:false
+
+cd %initialPath%
