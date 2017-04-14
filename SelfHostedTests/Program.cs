781ce6f4c1be3c8e49d5537a3a781313c8e48e90
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2017-04-14 13:31:18 +0200

    WIP: appveyor build: now tests all 6 tests assemblies for .net classic. disabled testing for core

---
+++ SelfHostedTests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SelfHostedTests
+{
+    using System.Reflection;
+    using NUnit.Common;
+    using NUnitLite;
+
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return new AutoRun(typeof(Program).GetTypeInfo().Assembly)
+                .Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
+        }
+    }
+}
