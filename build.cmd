1f5c2861675b00ac753db4d5d2f83d17a9e49ff5
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2017-04-29 10:18:12 +0200

    CI: build.cmd is now able to generate pretty names for non release version, supports nightly builds, release candidates and full release

--- build.cmd
+++ build.cmd
@@ -1,9 +1,16 @@
 @echo Off
+rem Trigger build process for NFluent
+rem usage build [Target] [Configuration]. Configuration is Release by default
+Rem target is either 'nightly' (default) for nightly build, 'RC' for release candidate, 'release' for release package
+Rem those targets build a tested packaged and pushed it on nuget (assuming a publishing key has been setup)
+Rem other targets (e.G. CI) build 'alpha package'
 cls
-set config=%1
-if "%config%" == "" (
-   set config=release
+set target=%1
+if "%target%"=="" (
+	set target=Nightly
 )
-
-REM msbuild .\.build\Build.proj /p:Configuration="%config%" /t:RunAll /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Diagnostic /nr:false
-msbuild .\.build\Build.proj /p:Configuration="%config%" /t:Package /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Diagnostic /nr:false
+set config=%2
+if "%config%" == "" (
+   set config=Release
+)
+msbuild .build\Build.proj /t:%target% /p:Configuration="%config%" /fl /flp:LogFile=msbuild.log;Verbosity=normal /nr:false
