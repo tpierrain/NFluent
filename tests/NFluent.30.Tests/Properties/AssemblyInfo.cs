50ecf77251c5263639515af256d7ddee0734199b
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2017-06-03 11:42:47 +0200

    Add doc generation for 2.0 and 3.0 targets Fixed naming overlap for Net 3.0 assemblies

--- tests/NFluent.30.Tests/Properties/AssemblyInfo.cs
+++ tests/NFluent.30.Tests/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NFluent.30.Tests")]
+[assembly: AssemblyTitle("NFluent.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
