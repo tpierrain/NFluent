cc0cc7572d0253af4f3dea9926db7281bc386a18
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2018-01-05 23:31:14 +0100

    CI: increase patch version number and change copyright to 2018

--- ForNCrunchUsers.md
+++ ForNCrunchUsers.md
@@ -9,5 +9,4 @@ To do so:
 Within the 'NCrunch Configuration' tab, set 'true' for the option: General->'Ignore this component completely' for all projects BUT NFluent.45, NFluent.45.Tests, NFluent.Tests.Extensions.45
 
 
-
 (*): The NFluent.45 project is linked to the NFluent.35 one through the 'Project Linker 2012' Visual Studio extension.
--- Version.cs
+++ Version.cs
@@ -26,9 +26,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
-[assembly: AssemblyInformationalVersion("2.1.1-alpha-0000")]
+[assembly: AssemblyVersion("2.1.2.0")]
+[assembly: AssemblyFileVersion("2.1.2.0")]
+[assembly: AssemblyInformationalVersion("2.1.2-alpha-0000")]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -37,7 +37,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Thomas PIERRAIN (thomas@pierrain.net), Cyrille DUPUYDAUBY (@cyrdup), Rui CARVALHO, Marc-Antoine LATOUR and Co")]
 [assembly: AssemblyProduct("NFluent")]
-[assembly: AssemblyCopyright("Copyright ©  2014-2017. Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0).")]
+[assembly: AssemblyCopyright("Copyright ©  2014-2018. Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0).")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
