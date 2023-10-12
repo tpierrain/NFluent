1519e035a8167f052529ddedb4c0724f9cdf7c68
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2023-10-12 13:28:37 +0200

    ci: update ver

--- code/.build/Version.cs
+++ code/.build/Version.cs
@@ -24,9 +24,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("3.0.1.0")]
-[assembly: AssemblyVersion("3.0.1.0")]
-[assembly: AssemblyInformationalVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.2.0")]
+[assembly: AssemblyVersion("3.0.2.0")]
+[assembly: AssemblyInformationalVersion("3.0.2.0")]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
