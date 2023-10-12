e8a2cd01f9b1831505e0f9aa8d744eecb0ce117e
Cyrille DUPUYDAUBY <dupdob@gmail.com>
2023-10-12 14:07:48 +0200

    ci: misc fixes

--- code/.build/Version.cs
+++ code/.build/Version.cs
@@ -25,8 +25,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("3.0.2.0")]
-[assembly: AssemblyVersion("3.0.2.0")]
-[assembly: AssemblyInformationalVersion("3.0.2.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.2.0-alpha")]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
